4a93ada1a5c44c32bf58f90fd432d5298bc7a81b
Mr-Rupam <devilbornname@gmail.com>
2024-06-16 14:20:00 +0530

    tryua[pm

--- src/components/HomePage/Environment/Environment.jsx
+++ src/components/HomePage/Environment/Environment.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { motion } from "framer-motion"
 import Image from "next/image";
 
-const AboutUs = () => {
+const Environment = () => {
   return <div
   className="outer-container"
 >
@@ -27,7 +27,7 @@ const AboutUs = () => {
     }}
     className="flex flex-col items-center justify-center gap-3 w-1/2">
       <p className="text-xl font-semibold tracking-[0.3rem] font-MaleoTrials-Bold">
-       STARBUCKS - YOUR HOME AWAY FROM HOME
+      STARBUCK'S ENVIRONMENTAL COMMITMENT
       </p>
       <h1
         
@@ -36,20 +36,19 @@ const AboutUs = () => {
   
       </h1>
       <p className="text-lg leading-relaxed font-sodo-sans font-normal not-italic text-center z-[2] ">
-      From the moment you step in, you'll be greeted with a smile and a familiar warmth that makes you feel right at home. Our comfortable seating, free Wi-Fi, and personalised service ensure that whether you're catching up with friends, working, or simply taking a break, you'll always find your perfect spot at Starbucks.
+      Starbucks is committed to sustainability and helping the planet. We ethically source our coffee, caring for the environment and communities. We reduce waste with recyclable and reusable packaging and cut down on single-use plastics.
       </p>
       <div
-    
       className="!m-0 relative top-[-2rem] left-[6.125rem] h-auto w-[110vw] overflow-hidden flex flex-row items-start justify-start z-[0]">
       <h1 
-      className="m-0 flex-1 h-auto relative tracking-[0.23em] leading-[5rem] font-normal font-inherit inline-block whitespace-nowrap max-w-full mbMedium:text-[5.625rem] mbMedium:leading-[5rem] mbMini:text-[1.563rem] mbMini:leading-[2rem] text-[#FAF6F3]">
-            Experience coffee like home
+    className="m-0 flex-1 h-auto relative  tracking-[0.23em] leading-[5rem] font-normal font-inherit inline-block whitespace-nowrap max-w-full mbMedium:text-[5.625rem] mbMedium:leading-[5rem] mbMini:text-[1.563rem] mbMini:leading-[2rem] text-[#FAF6F3]">
+            We care about the Air we breath
           </h1>
           </div>
       
       <div className="box-border relative  top-[-5rem] w-[32.313rem] h-[1.063rem] border-t-[1px] border-solid border-color-700 z-[5]" />
       <p className="mt-[-5rem] text-lg leading-relaxed font-sodo-sans font-normal  not-italic text-center z-[2]">
-      Instead of many components, we offer one seamless experience. Quality beans are sourced ethically, expert baristas craft each drink to your taste, and our welcoming stores provide a cozy atmosphere for you to relax and enjoy your coffee.
+      We invest in renewable energy and make our stores energy-efficient. Starbucks also supports reforestation and water conservation projects, working towards a greener future for everyone.
       </p>
 
     </motion.div>
@@ -63,4 +62,4 @@ const AboutUs = () => {
 </div>
 };
 
-export default AboutUs;
+export default Environment;
